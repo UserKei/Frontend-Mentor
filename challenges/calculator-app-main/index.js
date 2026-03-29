7fe78e0be0e0230f9a190881da62854fc2a58ff8
User_Kei <user_kei@outlook.com>
2026-03-30 00:26:21 +0800

    refactor(calculator): split calculator input handling

--- challenges/calculator-app-main/index.js
+++ challenges/calculator-app-main/index.js
@@ -4,59 +4,121 @@ const keypad = document.querySelector('.keypad');
 let currentValue = '0';
 let previousValue = '0';
 let operator;
-let shouleResetScreen = false;
+let shouldResetScreen = false;
 
 function render() {
   screen.textContent = currentValue;
 }
 
-function save(value) {
-  operator = value;
-  previousValue = currentValue;
-  shouleResetScreen = true;
-}
-
 function calculate(previousValue, operator, currentValue) {
   const left = Number(previousValue);
   const right = Number(currentValue);
+  let result = 0;
+  switch (operator) {
+    case '+':
+      result = left + right;
+      break;
+    case '-':
+      result = left - right;
+      break;
+    case '*':
+      result = left * right;
+      break;
+    case '/':
+      if (right === 0)
+        result = NaN;
+      else
+        result = left / right;
+      break;
+    default:
+      result = undefined;
+  }
+  return result.toString();
 }
 
 render();
 
-keypad.addEventListener('click', (e) => {
-  // console.log(e.target.matches('.key'))
-  if (e.target.matches('.key')) {
-    // console.log(e.target.textContent)
-    const value = e.target.textContent.trim();
-    if (value === 'RESET') {
-      currentValue = '0';
-    } else if (value === 'DEL') {
-      if (currentValue.length > 1) {
-        currentValue = currentValue.substring(0, currentValue.length - 1);
-      } else {
-        currentValue = '0';
-      }
-    } else if (value === '.') {
-      const hasDot = currentValue.includes('.');
-      if (!hasDot) currentValue += '.';
-    }
-    // ---- Number ----
-    else if ('0' <= value && value <= '9') {
-      if (shouleResetScreen) {
-        shouleResetScreen = false;
-        currentValue = '0';
-      }
-      if (currentValue === '0') {
-        currentValue = value;
-      } else {
-        currentValue += value;
-      }
-    }
-    else if (value === '=') {
-
-    } else {
-      save(value);
-    }
-    render();
+function handleInput(value) {
+  // Number or Dot
+  if (value >= '0' && value <= '9' || value === '.') {
+    handleNumberInput(value);
   }
-});
+
+  // Operator
+  else if (value === '+' || value === '-' || value === '*' || value === '/') {
+    handleOperatorInput(value);
+  }
+
+  // Equals
+  else if (value === '=') {
+    handleEquals();
+  }
+
+  // DEL
+  else if (value === 'DEL') {
+    if (currentValue.length > 1)
+      currentValue = currentValue.substring(0, currentValue.length - 1);
+    else
+      currentValue = '0';
+  }
+
+  // RESET
+  else if (value === 'RESET') {
+    currentValue = '0';
+    previousValue = '0';
+    operator = '';
+    shouldResetScreen = false;
+  }
+
+  render();
+}
+
+function handleNumberInput(value) {
+  if (value === '.') {
+    if (shouldResetScreen) {
+      currentValue = '0.';
+      shouldResetScreen = false;
+      return;
+    }
+
+    if (currentValue.includes('.')) {
+      return;
+    }
+
+    currentValue += '.';
+    return;
+  }
+
+  if (shouldResetScreen) {
+    currentValue = value;
+    shouldResetScreen = false;
+    return;
+  }
+
+  if (currentValue === '0') {
+    currentValue = value;
+  } else {
+    currentValue += value;
+  }
+
+  return;
+}
+
+function handleOperatorInput(value) {
+  if (shouldResetScreen) {
+    operator = value;
+    return;
+  }
+  shouldResetScreen = true;
+  previousValue = currentValue;
+  operator = value;
+}
+
+function handleEquals() {
+  currentValue = calculate(previousValue, operator, currentValue);
+  previousValue = currentValue;
+  operator = '';
+  shouldResetScreen = true;
+}
+
+
