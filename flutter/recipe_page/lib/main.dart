d2765ecd0ef953bfcb58fe38a4175967fba2902b
User_Kei <user_kei@outlook.com>
2026-04-05 20:37:34 +0800

    feat(recipe-page): add initial recipe page content

--- flutter/recipe_page/lib/main.dart
+++ flutter/recipe_page/lib/main.dart
@@ -9,10 +9,36 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Fuck you'),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child: Column(
+                  children: [
+                    Image.asset('assets/images/image-omelette.jpeg'),
+                    Padding(
+                      padding: EdgeInsetsGeometry.symmetric(),
+                      child: Column(
+                        children: [
+                          Text('Simple Omelette Recipe'),
+                          Text(
+                            'An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.',
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(),
+                      child: Column(children: []),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
