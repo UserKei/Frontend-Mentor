55421c79f8ff5bc3df943af3020df5b6041c5ee4
User_Kei <user_kei@outlook.com>
2026-04-09 16:42:10 +0800

    feat(ios/practice): add daily forecast view

--- ios/RecipePage/RecipePage/ContentView.swift
+++ ios/RecipePage/RecipePage/ContentView.swift
@@ -9,11 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
+    HStack {
+      DayForcast(day: "Mon", isRainy: false, high: 70, low: 50)
+      DayForcast(day: "Tun", isRainy: true, high: 60, low: 40)
+    }
+  }
+}
+
+struct DayForcast: View {
+  let day: String
+  let isRainy: Bool
+  let high: Int
+  let low: Int
+
+  var iconName: String {
+    if isRainy {
+      return "cloud.rain.fill"
+    } else {
+      return "sun.max.fill"
+    }
+  }
+
+  var iconColor: Color {
+    if isRainy {
+      return Color.blue
+    } else {
+      return Color.yellow
+    }
+  }
+
+  var body: some View {
     VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+      Text(day)
+        .font(Font.headline)
+      Image(systemName: iconName)
+        .foregroundStyle(iconColor)
+        .font(Font.largeTitle)
+        .padding()
+      Text("High: \(high)")
+      Text("Low: \(low)")
     }
     .padding()
   }
