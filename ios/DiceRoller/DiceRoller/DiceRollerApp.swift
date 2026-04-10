f280ff7c1b219d3eea250a9339bf764fb4a540e0
User_Kei <user_kei@outlook.com>
2026-04-10 13:22:10 +0800

    feat(ios): replace RecipePage with DiceRoller starter app

---
+++ ios/DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by User_Kei on 2026/4/10.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
